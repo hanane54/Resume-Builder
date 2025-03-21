f62cdb87d74b9fc021d917db834e770d7f8b39d6
TAOUIL Hanane <hanane59taouil@gmail.com>
2025-03-21 20:19:51 +0000

    Add postgresql driver dependency

--- src/main/java/com/resumebuilder/resumebuilder/controller/UserController.java
+++ src/main/java/com/resumebuilder/resumebuilder/controller/UserController.java
@@ -9,13 +9,11 @@ import com.resumebuilder.resumebuilder.service.UserService;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
