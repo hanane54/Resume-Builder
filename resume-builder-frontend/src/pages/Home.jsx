13fd7932efd15e65189b0377bfaaf5c5a24fd488
TAOUIL Hanane <hanane59taouil@gmail.com>
2025-03-25 22:47:57 +0000

    Added Navbar component

---
+++ resume-builder-frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Hanane's Resume Builder ^_^</h1>
+      <p>Create and manage your resumes easily</p>
+    </div>
+  );
+};
+
+export default Home;
